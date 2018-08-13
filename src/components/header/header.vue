fa483c0bab583666998843297994db1f78e204ba
wurong <wurong@shanhs.com>
2018-08-13 20:01:39 +0800

    init

---
+++ src/components/header/header.vue
@@ -0,0 +1,20 @@
+<template lang='pug'>
+.header-content-wrapper
+    .header-back
+    .header-input
+    .header-city
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang='less' scoped>
+.header-content-wrapper{
+    width:100%;
+    height: 0;
+    padding-bottom: 45px;
+    background-color: aqua;
+}
+
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+.home-content-wrapper
+    home-header
+
+</template>
+<script>
+import HomeHeader from '@/components/header/header'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    HomeHeader
+  }
+
+}
+</script>
+<style lang='less'>
+
+</style>
